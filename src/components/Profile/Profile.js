d52454a06413fd4663d178f8e957781ae01db8ab
João Victor Porto <joaovportog@gmail.com>
2025-10-21 17:55:24 -0300

    fix image

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { scrollToNextSection } from '../../Utils/Scroll';
-import profileImage from '../assets/images/joao1.jpeg';
+import profileImage from '../assets/images/joao2.png';
 import paraBaixo from '../assets/images/seta-para-baixo.png';
 import '../Buttons/ButtonStyles.scss';
 import './ProfileStyles.scss';
